a4457741cef9e4eafa27eedb3034a8caf0cc1b28
christinezchan <christinezchan@gmail.com>
2025-01-01 18:41:51 -0500

    add test mine floor

---
+++ HairyPotteryAndTheKilnOfFire/tileset/CLM_MainLev.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="CLM_MainLev" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
+ <image source="C:/Users/chris/Documents/drive-download-20241230T051111Z-001/craftlandmines/32x32/CLM_MainLev.png" width="2048" height="2048"/>
+</tileset>
